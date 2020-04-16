ddc52d272fafc49db560719adcd5d65b3fc65883
Emmanuel Marty <emmanuel@fgl.com>
2020-04-16 09:55:48 +0200

    Bump version

--- src/apultra.c
+++ src/apultra.c
@@ -45,7 +45,7 @@
 #define OPT_VERBOSE        1
 #define OPT_STATS          2
 
-#define TOOL_VERSION "1.0.9"
+#define TOOL_VERSION "1.1.0"
 
 /*---------------------------------------------------------------------------*/
 
